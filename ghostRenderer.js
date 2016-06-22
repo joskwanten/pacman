f42d08dd65f6929ccab411c52c7251b0b1f0b012
kwanten.j@tkppensioen.nl <kwante.j@PC2532.intra.tkppensioen.nl>
2016-06-22 16:33:27 +0200

    Energizers and points added to the maze.

--- ghostRenderer.js
+++ ghostRenderer.js
@@ -1,130 +1,130 @@
 function ghostRenderer(context, tileSize) {
 
-	//
-	this.underoff = 0;
-	this.underoffup = true;
+    //
+    this.underoff = 0;
+    this.underoffup = true;
 
-	if (context != null) {
-		context.lineWidth = 1;
-	}
+    if (context != null) {
+        context.lineWidth = 1;
+    }
 
-	var ghostSize = tileSize * 2;
+    var ghostSize = tileSize * 2;
 
-	this.render = function(ghost) {
+    this.render = function (ghost) {
 
-		this.x = ghost.x - tileSize / 2;
-		this.y = ghost.y - tileSize / 2;
+        this.x = ghost.x - tileSize / 2;
+        this.y = ghost.y - tileSize / 2;
 
-		if (ghost.color == 'blue') {
-			this.fill = ctx.createLinearGradient(this.x, this.y, this.x + ghostSize, this.y + ghostSize);
-			this.fill.addColorStop(0, '#8ED6FF');
-			this.fill.addColorStop(1, '#444CB3');
-		}
+        if (ghost.color == 'blue') {
+            this.fill = ctx.createLinearGradient(this.x, this.y, this.x + ghostSize, this.y + ghostSize);
+            this.fill.addColorStop(0, '#8ED6FF');
+            this.fill.addColorStop(1, '#444CB3');
+        }
 
-		if (ghost.color == 'red') {
-			this.fill = ctx.createLinearGradient(this.x, this.y, this.x + ghostSize, this.y + ghostSize);
-			this.fill.addColorStop(0, '#FFD68E');
-			this.fill.addColorStop(1, '#B34C44');
-		}
+        if (ghost.color == 'red') {
+            this.fill = ctx.createLinearGradient(this.x, this.y, this.x + ghostSize, this.y + ghostSize);
+            this.fill.addColorStop(0, '#FFD68E');
+            this.fill.addColorStop(1, '#B34C44');
+        }
 
-		if (ghost.color == 'yellow') {
-			this.fill = ctx.createLinearGradient(this.x, this.y, this.x + ghostSize, this.y + ghostSize);
-			this.fill.addColorStop(0, '#FFFF8E');
-			this.fill.addColorStop(1, '#B3BC44');
-		}
+        if (ghost.color == 'yellow') {
+            this.fill = ctx.createLinearGradient(this.x, this.y, this.x + ghostSize, this.y + ghostSize);
+            this.fill.addColorStop(0, '#FFFF8E');
+            this.fill.addColorStop(1, '#B3BC44');
+        }
 
-		if (ghost.color == 'green') {
-			this.fill = ctx.createLinearGradient(this.x, this.y, this.x + ghostSize, this.y + ghostSize);
-			this.fill.addColorStop(0, '#8EFF8E');
-			this.fill.addColorStop(1, '#44BC44');
-		}
+        if (ghost.color == 'green') {
+            this.fill = ctx.createLinearGradient(this.x, this.y, this.x + ghostSize, this.y + ghostSize);
+            this.fill.addColorStop(0, '#8EFF8E');
+            this.fill.addColorStop(1, '#44BC44');
+        }
 
-		if (this.underoffup) {
-			this.underoff += 2;
+        if (this.underoffup) {
+            this.underoff += 2;
 
-			if (this.underoff > 8)
-				this.underoffup = false;
-		}
-		else {
-			this.underoff -= 2;
+            if (this.underoff > 8)
+                this.underoffup = false;
+        }
+        else {
+            this.underoff -= 2;
 
-			if (this.underoff < -8)
-				this.underoffup = true;
-		}
+            if (this.underoff < -8)
+                this.underoffup = true;
+        }
 
-		ctx.beginPath();
-		ctx.moveTo(this.x + 0, this.y + tileSize);
-		ctx.bezierCurveTo(this.x + 0, this.y + 0, this.x + ghostSize, this.y + 0, this.x + ghostSize, this.y + tileSize);
-		//ctx.lineTo(this.x + 64, this.y + 48);
+        ctx.beginPath();
+        ctx.moveTo(this.x + 0, this.y + tileSize);
+        ctx.bezierCurveTo(this.x + 0, this.y + 0, this.x + ghostSize, this.y + 0, this.x + ghostSize, this.y + tileSize);
+        //ctx.lineTo(this.x + 64, this.y + 48);
 
-		ctx.bezierCurveTo(this.x+ this.underoff  + 1    * ghostSize, this.y + ghostSize, this.x + this.underoff + 0.75 * ghostSize, this.y + ghostSize, this.x + 0.75 * ghostSize, this.y + 0.75 * ghostSize);
-		ctx.bezierCurveTo(this.x + this.underoff + 0.75 * ghostSize, this.y + ghostSize, this.x + this.underoff + 0.5  * ghostSize, this.y + ghostSize, this.x + 0.5  * ghostSize, this.y + 0.75 * ghostSize);
-		ctx.bezierCurveTo(this.x + this.underoff + 0.5  * ghostSize, this.y + ghostSize, this.x + this.underoff + 0.25 * ghostSize, this.y + ghostSize, this.x + 0.25 * ghostSize, this.y + 0.75 * ghostSize);
-		ctx.bezierCurveTo(this.x + this.underoff + 0.25 * ghostSize, this.y + ghostSize, this.x + this.underoff + 0    * ghostSize, this.y + ghostSize, this.x + 0    * ghostSize, this.y + 0.5  * ghostSize);
+        ctx.bezierCurveTo(this.x + this.underoff + 1 * ghostSize, this.y + ghostSize, this.x + this.underoff + 0.75 * ghostSize, this.y + ghostSize, this.x + 0.75 * ghostSize, this.y + 0.75 * ghostSize);
+        ctx.bezierCurveTo(this.x + this.underoff + 0.75 * ghostSize, this.y + ghostSize, this.x + this.underoff + 0.5 * ghostSize, this.y + ghostSize, this.x + 0.5 * ghostSize, this.y + 0.75 * ghostSize);
+        ctx.bezierCurveTo(this.x + this.underoff + 0.5 * ghostSize, this.y + ghostSize, this.x + this.underoff + 0.25 * ghostSize, this.y + ghostSize, this.x + 0.25 * ghostSize, this.y + 0.75 * ghostSize);
+        ctx.bezierCurveTo(this.x + this.underoff + 0.25 * ghostSize, this.y + ghostSize, this.x + this.underoff + 0 * ghostSize, this.y + ghostSize, this.x + 0 * ghostSize, this.y + 0.5 * ghostSize);
 
-		ctx.fillStyle = this.fill;
-		ctx.fill();
-		//ctx.lineTo(this.x + 0, this.y + 32);
-		ctx.lineWidth = 1;
+        ctx.fillStyle = this.fill;
+        ctx.fill();
+        //ctx.lineTo(this.x + 0, this.y + 32);
+        ctx.lineWidth = 1;
 
-		// line color
-		ctx.strokeStyle = ghost.color;
-		ctx.stroke();
+        // line color
+        ctx.strokeStyle = ghost.color;
+        ctx.stroke();
 
-		// Draw ghost eyes
-		ctx.beginPath();
-		ctx.arc(this.x + (18 / 64) * ghostSize, this.y + (28/ 64) * ghostSize, (7 / 64) * ghostSize, 0, 2 * Math.PI, false);
-		ctx.fillStyle = 'white';
-		ctx.fill();
-		ctx.lineWidth = 1;
-		ctx.strokeStyle = '#003300';
-		ctx.stroke();
+        // Draw ghost eyes
+        ctx.beginPath();
+        ctx.arc(this.x + (18 / 64) * ghostSize, this.y + (28 / 64) * ghostSize, (7 / 64) * ghostSize, 0, 2 * Math.PI, false);
+        ctx.fillStyle = 'white';
+        ctx.fill();
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = '#003300';
+        ctx.stroke();
 
-		ctx.beginPath();
-		ctx.arc(this.x + (42 / 64) * ghostSize, this.y + (28 / 64) * ghostSize, (7 / 64) * ghostSize, 0, 2 * Math.PI, false);
-		ctx.fillStyle = 'white';
-		ctx.fill();
-		ctx.lineWidth = 1;
-		ctx.strokeStyle = '#003300';
-		ctx.stroke();
+        ctx.beginPath();
+        ctx.arc(this.x + (42 / 64) * ghostSize, this.y + (28 / 64) * ghostSize, (7 / 64) * ghostSize, 0, 2 * Math.PI, false);
+        ctx.fillStyle = 'white';
+        ctx.fill();
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = '#003300';
+        ctx.stroke();
 
-		// Draw ghost pupils
-		var offsetx = 0;
-		var offsety = 0;
+        // Draw ghost pupils
+        var offsetx = 0;
+        var offsety = 0;
 
-		switch(ghost.direction) {
-			case "up":
-				offsety = -2
-				break;
-			case "down":
-				offsety = 2;
-				break;
-			case "left":
-				offsetx = -2;
-				break;
-			case "right":
-				offsetx = 2;
-				break;
+        switch (ghost.direction) {
+            case "up":
+                offsety = -2
+                break;
+            case "down":
+                offsety = 2;
+                break;
+            case "left":
+                offsetx = -2;
+                break;
+            case "right":
+                offsetx = 2;
+                break;
 
-		}
+        }
 
-		var pupilSize = (3 / 64) * ghostSize;
-		pupilSize = pupilSize < 1 ? 1 : pupilSize;
+        var pupilSize = (3 / 64) * ghostSize;
+        pupilSize = pupilSize < 1 ? 1 : pupilSize;
 
-		ctx.beginPath();
-		ctx.arc(this.x + offsetx + (18 / 64) * ghostSize, this.y + offsety + (28 / 64) * ghostSize, pupilSize, 0, 2 * Math.PI, false);
-		ctx.fillStyle = 'black';
-		ctx.fill();
-		ctx.lineWidth = 1;
-		ctx.strokeStyle = '#003300';
-		ctx.stroke();
+        ctx.beginPath();
+        ctx.arc(this.x + offsetx + (18 / 64) * ghostSize, this.y + offsety + (28 / 64) * ghostSize, pupilSize, 0, 2 * Math.PI, false);
+        ctx.fillStyle = 'black';
+        ctx.fill();
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = '#003300';
+        ctx.stroke();
 
-		ctx.beginPath();
-		ctx.arc(this.x + offsetx + (42 / 64) * ghostSize, this.y + offsety + (28 / 64) * ghostSize, pupilSize, 0, 2 * Math.PI, false);
-		ctx.fillStyle = 'black';
-		ctx.fill();
-		ctx.lineWidth = 1;
-		ctx.strokeStyle = '#003300';
-		ctx.stroke();
-	}
+        ctx.beginPath();
+        ctx.arc(this.x + offsetx + (42 / 64) * ghostSize, this.y + offsety + (28 / 64) * ghostSize, pupilSize, 0, 2 * Math.PI, false);
+        ctx.fillStyle = 'black';
+        ctx.fill();
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = '#003300';
+        ctx.stroke();
+    }
 }
--- pacman.js
+++ pacman.js
@@ -1,114 +1,116 @@
 /* Width and Height */
-var W=896, H=1152, tileSize =32, horizontalTiles = 28, verticalTiles = 36;
+var W = 896, H = 1152, tileSize = 32, horizontalTiles = 28, verticalTiles = 36;
 
 /* List of ids that can be used in the tiles */
-var ElementIDs = { 
-	BLANK:  		0, /* Blank tile */
-	DTLCORNER:  1, /* Double lined top-left corner */
-  DTRCORNER:  2, /* Double lined top-right corner */
-  DBLCORNER:  3, /* Double lined bottom-left corner */
-  DBRCORNER:  4, /* Double lined bottom-right corner */
-  DHORLINET:  5, /* Double line horizontal top*/
-  DHORLINEB:  6, /* Double line horizontal bottom*/
-  DVERTLINEL: 7, /* Double line vertical left */
-  DVERTLINER: 8, /* Double line vertical rigth */
-  STLCORNER:  9, /* Single lined top-left corner */
-  STRCORNER: 10, /* Single lined top-right corner */
-  SBLCORNER: 11, /* Single lined bottom-left corner */
-  SBRCORNER: 12, /* Single lined bottom-right corner */
-  SVERTLINE: 13, /* Single vertical line */
-  SHORLINE:  14, /* Single horizontal line */
-  PELLET:    15, /* Pellet to eat by pacman */
-  ENERGIZER: 16, /* Energizer pellet after eating pacman can defeat ghosts */
-  BLINKY:    17, /* Blinky the red ghost */
-  PINKY:     18, /* Pinky the pink ghost */
-  INKY:      19, /* Inky the blue ghost */
-  CLYDE:     20,  /* Clyde the slow ghost */    
-  DHORLINESVERTTL: 21, 
-  DHORLINESVERTTR: 22, 
-  DHORLINESVERTBL: 23, 
-  DHORLINESVERTBR: 24,
-  DTRCORNERBL: 25,
-  DBRCORNERTL: 26,
-  DTLCORNERBL: 27,
-  DBLCORNERTL: 28,
-  DVERTTOHORSTL: 29,
-  DVERTTOHORSBL: 30,
-  DVERTTOHORSTR: 31,
-  DVERTTOHORSBR: 32
+var ElementIDs = {
+    BLANK: 0, /* Blank tile */
+    DTLCORNER: 1, /* Double lined top-left corner */
+    DTRCORNER: 2, /* Double lined top-right corner */
+    DBLCORNER: 3, /* Double lined bottom-left corner */
+    DBRCORNER: 4, /* Double lined bottom-right corner */
+    DHORLINET: 5, /* Double line horizontal top*/
+    DHORLINEB: 6, /* Double line horizontal bottom*/
+    DVERTLINEL: 7, /* Double line vertical left */
+    DVERTLINER: 8, /* Double line vertical rigth */
+    STLCORNER: 9, /* Single lined top-left corner */
+    STRCORNER: 10, /* Single lined top-right corner */
+    SBLCORNER: 11, /* Single lined bottom-left corner */
+    SBRCORNER: 12, /* Single lined bottom-right corner */
+    SVERTLINE: 13, /* Single vertical line */
+    SHORLINE: 14, /* Single horizontal line */
+    PELLET: 15, /* Pellet to eat by pacman */
+    ENERGIZER: 16, /* Energizer pellet after eating pacman can defeat ghosts */
+    BLINKY: 17, /* Blinky the red ghost */
+    PINKY: 18, /* Pinky the pink ghost */
+    INKY: 19, /* Inky the blue ghost */
+    CLYDE: 20, /* Clyde the slow ghost */
+    DHORLINESVERTTL: 21,
+    DHORLINESVERTTR: 22,
+    DHORLINESVERTBL: 23,
+    DHORLINESVERTBR: 24,
+    DTRCORNERBL: 25,
+    DBRCORNERTL: 26,
+    DTLCORNERBL: 27,
+    DBLCORNERTL: 28,
+    DVERTTOHORSTL: 29,
+    DVERTTOHORSBL: 30,
+    DVERTTOHORSTR: 31,
+    DVERTTOHORSBR: 32,
+    ENERGIZER: 64,
+    POINT: 65
 }
 
 function Ghost(id, name, color, character, maze, tileSize) {
-	this.id = id;
-	this.name = name;
-	this.color = color;
-	this.character = character;
-	this.maze = maze;
-	// speed in frames to travel to the next tile
-	this.speed = 10;
-	
-	
-	this.pointInMazeV = verticalTiles / 2;
-	this.pointInMazeH = horizontalTiles / 2;
-	
-	this.x = 320;
-	this.y = 320;
-	
-	this.direction = "left";
-	
-	
-	this.update = function() {
-		if (this.x % tileSize == 0 && this.y % tileSize == 0) {
-			this.pointInMazeH = this.x / tileSize;
-			this.pointInMazeV = this.y / tileSize;
-			
-			var upAllowed    = this.pointInMazeV > 0                   ? maze[(this.pointInMazeV - 1) * horizontalTiles + this.pointInMazeH] == 0 : false;
-			var downAllowed  = this.pointInMazeV < verticalTiles - 1   ? maze[(this.pointInMazeV + 1) * horizontalTiles + this.pointInMazeH] == 0 : false;
-			var leftAllowed  = this.pointInMazeH > 0                   ? maze[this.pointInMazeV  * horizontalTiles + (this.pointInMazeH - 1)] == 0 : false;
-			var rightAllowed = this.pointInMazeH < horizontalTiles - 1 ? maze[this.pointInMazeV  * horizontalTiles + this.pointInMazeH + 1] == 0 : false;
-			
-			if (upAllowed && this.direction == 'up' || downAllowed && this.direction == 'down' || leftAllowed && this.direction == 'left' || rightAllowed && this.direction == 'right') {
-				// do nothing 
-			} else {
-				var allowedDirections = [];
-			
-				if (upAllowed) {
-					allowedDirections.push("up");				
-				}
-			
-				if (downAllowed) {
-					allowedDirections.push("down");				
-				}
-			
-				if (leftAllowed) {
-					allowedDirections.push("left");				
-				}
-			
-				if (rightAllowed) {
-					allowedDirections.push("right");				
-				}
-			
-				this.direction = allowedDirections[Math.floor((Math.random()*allowedDirections.length))];
-			}
-		}
-				
-		switch(this.direction) {
-			case "right":
-				this.x += tileSize / 8;
-				break;
-			case "left":
-				this.x -= tileSize / 8;
-				break;
-			case "up":
-				this.y -= tileSize / 8;
-				break;
-			case "down": 
-				this.y += tileSize / 8;
-				break;
-		}				
-	}
-	
-	
+    this.id = id;
+    this.name = name;
+    this.color = color;
+    this.character = character;
+    this.maze = maze;
+    // speed in frames to travel to the next tile
+    this.speed = 10;
+
+
+    this.pointInMazeV = verticalTiles / 2;
+    this.pointInMazeH = horizontalTiles / 2;
+
+    this.x = 320;
+    this.y = 320;
+
+    this.direction = "left";
+
+
+    this.update = function () {
+        if (this.x % tileSize == 0 && this.y % tileSize == 0) {
+            this.pointInMazeH = this.x / tileSize;
+            this.pointInMazeV = this.y / tileSize;
+
+            var upAllowed = this.pointInMazeV > 0 ? maze[(this.pointInMazeV - 1) * horizontalTiles + this.pointInMazeH] > 63 : false;
+            var downAllowed = this.pointInMazeV < verticalTiles - 1 ? maze[(this.pointInMazeV + 1) * horizontalTiles + this.pointInMazeH] > 63 : false;
+            var leftAllowed = this.pointInMazeH > 0 ? maze[this.pointInMazeV * horizontalTiles + (this.pointInMazeH - 1)] > 63 : false;
+            var rightAllowed = this.pointInMazeH < horizontalTiles - 1 ? maze[this.pointInMazeV * horizontalTiles + this.pointInMazeH + 1] > 63 : false;
+
+            if (upAllowed && this.direction == 'up' || downAllowed && this.direction == 'down' || leftAllowed && this.direction == 'left' || rightAllowed && this.direction == 'right') {
+                // do nothing
+            } else {
+                var allowedDirections = [];
+
+                if (upAllowed) {
+                    allowedDirections.push("up");
+                }
+
+                if (downAllowed) {
+                    allowedDirections.push("down");
+                }
+
+                if (leftAllowed) {
+                    allowedDirections.push("left");
+                }
+
+                if (rightAllowed) {
+                    allowedDirections.push("right");
+                }
+
+                this.direction = allowedDirections[Math.floor((Math.random() * allowedDirections.length))];
+            }
+        }
+
+        switch (this.direction) {
+            case "right":
+                this.x += tileSize / 8;
+                break;
+            case "left":
+                this.x -= tileSize / 8;
+                break;
+            case "up":
+                this.y -= tileSize / 8;
+                break;
+            case "down":
+                this.y += tileSize / 8;
+                break;
+        }
+    }
+
+
 }
 
 var ghosts = [];
@@ -119,17 +121,17 @@ ghosts.push(new Ghost(ElementIDs.CLYDE, "CLYDE", "green", "POKEY", maze, tileSiz
 
 
 function Game(mazeData, renderFunction) {
-	this.renderFunction = renderFunction;
-    this.maze = mazeData;          
-    
-    this.update = function() {
-		ghosts.forEach(function(ghost) {
-			ghost.update();
-		})
+    this.renderFunction = renderFunction;
+    this.maze = mazeData;
+
+    this.update = function () {
+        ghosts.forEach(function (ghost) {
+            ghost.update();
+        })
     }
-    
-    this.render = function() {		
-			renderFunction(this.maze);      
+
+    this.render = function () {
+        renderFunction(this.maze);
     }
 }
 
@@ -145,19 +147,19 @@ var ghostRenderer3 = new ghostRenderer(ctx, tileSize);
 var ghostRenderer4 = new ghostRenderer(ctx, tileSize);
 
 /* Create a new Game */
-var myGame = new Game(maze, function(myMaze){ 
-	mazeRenderer.renderMaze(myMaze);
-	ghostRenderer1.render(this.ghosts[0]);
-	ghostRenderer2.render(this.ghosts[1]);
-	ghostRenderer3.render(this.ghosts[2]);
-	ghostRenderer4.render(this.ghosts[3]);
+var myGame = new Game(maze, function (myMaze) {
+    mazeRenderer.renderMaze(myMaze);
+    ghostRenderer1.render(this.ghosts[0]);
+    ghostRenderer2.render(this.ghosts[1]);
+    ghostRenderer3.render(this.ghosts[2]);
+    ghostRenderer4.render(this.ghosts[3]);
 });
 
 var fps = 60;
-setInterval(function() {
-		myGame.update();
-		window.requestAnimationFrame(function() {
-			myGame.render();  	
-		});        
+setInterval(function () {
+    myGame.update();
+    window.requestAnimationFrame(function () {
+        myGame.render();
+    });
 }, 1000 / fps);
 
